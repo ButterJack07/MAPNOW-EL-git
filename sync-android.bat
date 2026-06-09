9d5b87995578b48c75f8d29b6b0f9390319c0a29
ButterJack07 <zhenggaojian123456@163.com>
2026-06-09 12:58:59 +0800

    Delete sync-android.bat

--- sync-android.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set SRC=C:\Users\zheng\Desktop\此刻地图设计版\此刻地图成品\A1.0
-set DST=D:\ProgramData\AndroidStudio\AndroidStudioProjects\MomentMap\app\src\main\assets
-
-xcopy /E /Y "%SRC%\index.html" "%DST%\"
-xcopy /E /Y "%SRC%\css\*" "%DST%\css\"
-xcopy /E /Y "%SRC%\js\*" "%DST%\js\"
-echo 同步完成！
-pause
